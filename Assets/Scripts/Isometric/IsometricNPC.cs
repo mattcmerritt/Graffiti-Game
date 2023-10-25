c64fa6fc0c437d60a64db90d6e5e0241df308256
mattcmerritt <mattcmerritt@gmail.com>
2023-10-25 05:10:11 -0400

    added basic noncombat interactions and allowed players to interact with NPCs

---
+++ Assets/Scripts/Isometric/IsometricNPC.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricNPC : MonoBehaviour
+{
+    [SerializeField] private bool InteractTriggerHit;
+    [SerializeField] private Collider PreviousInteractor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If NPC is intended to have multiple collision boxes, add && !InteractTriggerHit
+        // and bring back commented lines
+        if (other.CompareTag("Player Hitbox"))
+        {
+            Interact();
+            // Multiple collision box additions
+            // InteractTriggerHit = true;
+            // PreviousInteractor = other;
+        }
+    }
+
+    // Mark that the interaction trigger is over once the collision has finished
+    // Prevents issues with interactions being triggered multiple times by the same collider
+    // Currently unused
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (InteractTriggerHit)
+    //     {
+    //         if (PreviousInteractor == other)
+    //         {
+    //             InteractTriggerHit = false;
+    //         }
+    //     } 
+    // }
+
+    private void Interact()
+    {
+        Debug.Log($"<color=yellow>Interactions: </color>{gameObject.name} NPC interacted with.");
+    }
+}
