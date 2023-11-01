2feeb3a6af5f391a860bff34fcca200e229a349b
mattcmerritt <mattcmerritt@gmail.com>
2023-11-01 06:26:18 -0400

    cooldowns added to the platforming scenes

---
+++ Assets/Scripts/Platforming/PlatformingUI.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformingUI : MonoBehaviour
+{
+    [SerializeField] private Image DashIndicator;
+    [SerializeField] private Image DoubleJumpIndicator;
+
+    // Internal data
+    private float DashCooldown, MaxDashCooldown;
+    private bool Paused;
+
+    private void Start()
+    {
+        PlatformingLevel.Instance.OnPlayerDash += UpdateDashIndicator;
+        PlatformingLevel.Instance.OnPlayerDoubleJump += () => UpdateDoubleJumpIndicator(false);
+        PlatformingLevel.Instance.OnPlayerDoubleJumpRefresh += () => UpdateDoubleJumpIndicator(true);
+        PlatformingLevel.Instance.OnPause += Pause;
+        PlatformingLevel.Instance.OnResume += Resume;
+    }
+
+    private void Update()
+    {
+        if (!Paused)
+        {
+            if (DashCooldown > 0f)
+            {
+                DashCooldown -= Time.deltaTime;
+                DashIndicator.fillAmount = 1 - DashCooldown / MaxDashCooldown;
+            }
+            else
+            {
+                DashIndicator.fillAmount = 1f;
+                // could play an animation here
+            }
+        }
+    }
+
+    private void UpdateDashIndicator(float cooldown)
+    {
+        DashCooldown = cooldown;
+        MaxDashCooldown = cooldown;
+    }
+
+    private void UpdateDoubleJumpIndicator(bool status)
+    {
+        if (status)
+        {
+            DoubleJumpIndicator.fillAmount = 1f;
+            // could play an animation here
+        }
+        else {
+            DoubleJumpIndicator.fillAmount = 0f;
+        }
+    }
+
+    private void Pause()
+    {
+        Paused = true;
+    }
+
+    private void Resume()
+    {
+        Paused = false;
+    }
+}
