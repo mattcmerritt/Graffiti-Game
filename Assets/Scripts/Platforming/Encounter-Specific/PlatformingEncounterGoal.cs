fd26dfa550bc0bd83af890d04f46171d0a24dc15
mikepmerritt <mikepmerritt@gmail.com>
2023-12-01 04:12:46 -0500

    added drip hazards, fixed follow cam and platforming encounter bugs

--- Assets/Scripts/Platforming/Encounter-Specific/PlatformingEncounterGoal.cs
+++ Assets/Scripts/Platforming/Encounter-Specific/PlatformingEncounterGoal.cs
@@ -6,7 +6,10 @@ public class PlatformingEncounterGoal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Debug.Log("The encounter has been cleared.");
-        PlatformingEncounter.Instance.ClearEncounterSuccessfully();
+        if(other.gameObject.GetComponent<PlatformingPlayer>() != null)
+        {
+            // Debug.Log("The encounter has been cleared.");
+            PlatformingEncounter.Instance.ClearEncounterSuccessfully();
+        }
     }
 }
