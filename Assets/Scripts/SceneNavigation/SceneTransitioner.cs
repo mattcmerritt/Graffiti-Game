a8f8d6cd095e5296636827c386a4ade0a3ff88e9
mikepmerritt <mikepmerritt@gmail.com>
2023-12-13 00:31:16 -0500

    game now repeats properly

--- Assets/Scripts/SceneNavigation/SceneTransitioner.cs
+++ Assets/Scripts/SceneNavigation/SceneTransitioner.cs
@@ -37,12 +37,18 @@ public class SceneTransitioner : MonoBehaviour
             TransitionActive = true;
             Animator.SetTrigger("Enter Stage (Good)");
             yield return new WaitForSeconds(TransitionDuration);
+            Destroy(BuddyManager.Instance.gameObject);
+            BuddyManager.Instance = null;
             SceneManager.LoadScene("MainMenu");
-            Animator.SetTrigger("Leave Stage (Good)");
-            yield return new WaitForSeconds(TransitionDuration);
-            Destroy(this.gameObject);
+            StartCoroutine(DestroyObjectOnReload());
         }
+    }
 
+    public IEnumerator DestroyObjectOnReload()
+    {
+        Animator.SetTrigger("Leave Stage (Good)");
+        yield return new WaitForSeconds(TransitionDuration);
+        Destroy(this.gameObject);
     }
 
     public IEnumerator StartGameTransition()
